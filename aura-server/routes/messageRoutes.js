17e806f518356bba222b367b061a9cb8a810d4ef
Max <maximrucom2@gmail.com>
2025-06-15 18:32:12 +1000

    fix: Correct message routes definition

--- aura-server/routes/messageRoutes.js
+++ aura-server/routes/messageRoutes.js
@@ -1,25 +1,32 @@
 // aura-server/routes/messageRoutes.js
 const express = require('express');
 const router = express.Router();
-const { createMessage, getMessages, markMessageAsRead, deleteMessage, addReply, getMyMessages } = require('../controllers/messageController');
+const { 
+    createMessage, 
+    getMessages, 
+    markMessageAsRead, 
+    deleteMessage, 
+    addReply, 
+    getMyMessages 
+} = require('../controllers/messageController');
 const { protect, admin } = require('../middleware/authMiddleware');
 
 router.route('/')
     .post(protect, createMessage)
     .get(protect, admin, getMessages);
 
+// --- ИЗМЕНЕНИЕ ---
+// Оставляем только один роут для /mymessages
 router.route('/mymessages').get(protect, getMyMessages);
-
-router.route('/:id')
-    .delete(protect, admin, deleteMessage);
 
 router.route('/:id/read')
     .put(protect, admin, markMessageAsRead);
 
-
 router.route('/:id/reply') 
     .post(protect, admin, addReply);
 
-router.route('/mymessages').get(protect, getMyMessages);
+// Роут /:id должен быть в конце, чтобы не перехватывать /mymessages
+router.route('/:id')
+    .delete(protect, admin, deleteMessage);
 
 module.exports = router;
